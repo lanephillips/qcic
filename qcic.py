5517434acc5d1bdfdc135fd9393186027b7dccc0
Lane Phillips <lane@milkllc.com>
2014-12-10 02:21:06 +0000

    record nonstop; delete when full

--- qcic.py
+++ qcic.py
@@ -1,13 +1,43 @@
 import picamera
 import datetime
+import os
+
+delcount = 2
+
+def check_fs():
+    global delcount
+
+    st = os.statvfs('/')
+    pct = 100 - st.f_bavail * 100.0 / st.f_blocks
+    print pct, "percent full"
+    
+    if pct > 90:
+        # less than 10% left, delete a few minutes
+        files = os.listdir('.')
+        files.sort()
+
+        for i in range(0, delcount):
+            print "deleting", files[i]
+            os.remove(files[i])
+        delcount += 1   # keep increasing until we get under 90%
+    else:
+        delcount = 2
 
 with picamera.PiCamera() as camera:
-    tstamp = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    camera.start_recording(tstamp + '.h264')
-    camera.wait_recording(5)
-    for i in range(0, 11):
+    try:
+        check_fs()
         tstamp = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
-        camera.split_recording(tstamp + '.h264')
-        camera.wait_recording(5)
+        print "recording", tstamp
+        camera.start_recording(tstamp + '.h264')
+        camera.wait_recording(60)
+        while True:
+            check_fs()
+            tstamp = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
+            print "recording", tstamp
+            camera.split_recording(tstamp + '.h264')
+            camera.wait_recording(60)
+
+    except KeyboardInterrupt:
+        print "quitting"
     camera.stop_recording()
 
